c239aac05606d23743ed8812d0f2a089dc516089
Sai Nihar <vishwaniharpendota@gmail.com>
2018-02-22 22:23:21 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sleep 05
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sleep 05
-sudo apt-get update
+sudo apt-get -y update
 sleep 10
-sudo apt-get install jenkins
+sudo apt-get -y install jenkins
 sleep 10
 sudo service jenkins start
