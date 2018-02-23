7cb3c582bc49540705c2688ffb34cb446501f102
Ubuntu <ubuntu@ip-172-31-22-11.ec2.internal>
2018-02-23 01:40:27 +0000

    script added

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sleep 05
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sleep 05
+sudo apt-get update
+sleep 10
+sudo apt-get install jenkins
+sleep 10
+sudo service jenkins start
